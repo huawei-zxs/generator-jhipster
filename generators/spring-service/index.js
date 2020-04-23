b69b2ad77550a6b99855424a6b3bba98336f8f4e
SWE-Rebench Builder <swe-rebench@local>
2020-04-23 09:03:45 +0200

    Materialize buggy state for gen_20260811114517_00296
    SWE-Rebench-Task-ID: gen_20260811114517_00296
    SWE-Rebench-Original-Base: b3c83b139e058827ac6d9913a100673cc15b9173
    SWE-Rebench-Patch-SHA256: 5a7b0d06c5a8bf6e2312c6eb5a912d77a4443cba70e6ced91e5d8a5fdb1e92e2

--- generators/spring-service/index.js
+++ generators/spring-service/index.js
@@ -129,7 +129,7 @@ module.exports = class extends BaseBlueprintGenerator {
                 if (this.useInterface) {
                     this.template(
                         `${this.fetchFromInstalledJHipster(
-                            'spring-service/templates'
+                            'spring-service/template'
                         )}/${SERVER_MAIN_SRC_DIR}package/service/impl/ServiceImpl.java.ejs`,
                         `${SERVER_MAIN_SRC_DIR + this.packageFolder}/service/impl/${this.serviceClass}Impl.java`
                     );
