1509923d99a343f1aff48f332ee6d908d35bd6c4
SWE-Rebench Builder <swe-rebench@local>
2020-04-23 09:03:45 +0200

    Materialize buggy state for gen_20260814091329_00438
    SWE-Rebench-Task-ID: gen_20260814091329_00438
    SWE-Rebench-Original-Base: b3c83b139e058827ac6d9913a100673cc15b9173
    SWE-Rebench-Patch-SHA256: 09bc257b3e6ba8983fb401f44d3331a965c531da2f9db7bf77053ca1d81a7786

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -52,9 +52,16 @@ describe('JHipster Utils', () => {
             const packageName = 'com.package';
             const angularAppName = 'myApp';
             const clientRootFolder = 'root';
-            const entity = { enumName: 'entityName', fieldValues: 'field1, field2' };
+            const entity = { fieldType: 'EntityName', fieldValues: 'field1,field2' };
             const infos = utils.buildEnumInfo(entity, angularAppName, packageName, clientRootFolder);
-            assert.objectContent(infos, { packageName, angularAppName, clientRootFolder: `${clientRootFolder}-` });
+            assert.objectContent(infos, {
+                enumName: 'EntityName',
+                enumInstance: 'entityName',
+                enumValues: 'field1, field2',
+                packageName,
+                angularAppName,
+                clientRootFolder: `${clientRootFolder}-`
+            });
         });
     });
     describe('::deepFind function', () => {
